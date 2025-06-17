85ed125a64c1570c898e18640cd410467076b5e0
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-06-18 01:33:30 +0600

    Add SVG assets and implement header components for weather app
    - Added SVG icons for pin, rainy, react, search, sun, and thunder.
    - Created Favourite component to display favourite locations.
    - Implemented FavouriteListModal to show a list of favourite locations.
    - Developed Header component to include logo, search, and favourite functionalities.
    - Added Logo component to render the application logo.
    - Created Search component for location search functionality.
    - Implemented AddToFavourite component to allow users to add locations to favourites.
    - Developed WeatherBoard component to display weather information and conditions.
    - Created WeatherCondition component to show detailed weather metrics.
    - Implemented WeatherHeadline component to display current temperature and location.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-red-300">Weather App</h1>
+    <div className="grid place-items-center h-screen">
+      <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
     </div>
   );
 }
