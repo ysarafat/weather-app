bd9c66a4a5d3f2be62d25fd359ad14b6fba44ddf
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-06-19 14:18:02 +0600

    Refactor Search component to remove local state and handle input changes with debounced search; add useDebounce hook for improved performance.

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -1,33 +1,30 @@
-import { useContext, useState } from "react";
-import SearchIcon from "../../assets/search.svg";
+import { useContext } from "react";
 import { LocationContext } from "../../context";
 import { getLocationByName } from "../../data/location-data";
+import useDebounce from "../../hooks/useDebounce";
 
 export default function Search() {
-  const [searchTerm, setSearchTerm] = useState("");
   const { setSelectedLocation } = useContext(LocationContext);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const fetchLocation = getLocationByName(searchTerm);
-
+  const doSearch = useDebounce((term) => {
+    const fetchLocation = getLocationByName(term);
     setSelectedLocation({ ...fetchLocation });
-  };
+  }, 500);
+
+  function handleChange(e) {
+    doSearch(e.target.value);
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
         <input
           className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
           type="search"
           placeholder="Search Location"
           required
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleChange}
         />
-        <button type="submit">
-          <img src={SearchIcon} />
-        </button>
       </div>
     </form>
   );
