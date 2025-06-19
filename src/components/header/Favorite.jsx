370fc6edfcdd695a4954312b3c6b973bffc14871
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-06-19 12:22:41 +0600

    Refactor Favorite component to accept onShow prop; update Header to manage favorite modal visibility and enhance FavoriteListModal to display favorites from context

--- src/components/header/Favorite.jsx
+++ src/components/header/Favorite.jsx
@@ -1,10 +1,12 @@
 import HeartIcon from "../../assets/heart.svg";
 
-export default function Favorite() {
+export default function Favorite({ onShow }) {
   return (
     <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
       <img src={HeartIcon} alt="" />
-      <span>Favorite Locations</span>
+      <button onClick={onShow} className="cursor-pointer">
+        Favorite Locations
+      </button>
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import Favorite from "./Favorite";
 import FavoriteListModal from "./FavoriteListModal";
 import Logo from "./Logo";
 import Search from "./Search";
 
 export default function Header() {
+  const [showFavModal, setShowFavModal] = useState(false);
+
   return (
     <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
       <nav className="container flex items-center justify-between py-6">
@@ -11,9 +14,9 @@ export default function Header() {
 
         <div className="flex items-center gap-4 relative">
           <Search />
-          <Favorite />
+          <Favorite onShow={() => setShowFavModal(!showFavModal)} />
           {/* <!-- Modal --> */}
-          <FavoriteListModal />
+          {showFavModal && <FavoriteListModal />}
         </div>
       </nav>
     </header>
